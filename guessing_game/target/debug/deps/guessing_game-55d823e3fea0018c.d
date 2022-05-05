b83925ee0979752384e422d0d37553ec6db7b8f4
anton-shablyko <shablyko@gmail.com>
2022-05-04 20:20:03 -0700

    clan gitignore

--- guessing_game/target/debug/deps/guessing_game-55d823e3fea0018c.d
+++
@@ -1,5 +0,0 @@
-/Users/mo0nz22/PycharmProjects/rustlang/guessing_game/target/debug/deps/guessing_game-55d823e3fea0018c: src/main.rs
-
-/Users/mo0nz22/PycharmProjects/rustlang/guessing_game/target/debug/deps/guessing_game-55d823e3fea0018c.d: src/main.rs
-
-src/main.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/Users/mo0nz22/PycharmProjects/rustlang/guessing_game/target/debug/guessing_game: /Users/mo0nz22/PycharmProjects/rustlang/guessing_game/src/main.rs
