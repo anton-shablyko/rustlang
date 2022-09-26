e478a62b1870c0bf50517924a0e7b27380321bcb
anton-shablyko <shablyko@gmail.com>
2022-09-26 13:16:54 -0700

    fizzbuzz=2

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -1,26 +1,43 @@
 use std::io;
 
 fn main() {
+    //my first attempt at fizzbuzz
     println!("Welcome to FizzBuzz!!\nEnter your number...");
 
     // get user's input as a string and convert it to u32
     let mut x = String::new();
     io::stdin().read_line(&mut x).expect("Failed to read line");
     let x: u32 = x.trim().parse().expect("Couldn't parse the number");
+    // basic(x)
+    using_match(x);
+}
 
-    for n in 1..x + 1 {
-        let mut r = "nope";
+fn basic(user_input: u32) {
+    for n in 1..=user_input {
+        let r;
 
-        if n % 3 == 0 {
-            r = "fizz"
-        }
-        if n % 5 == 0 {
-            r = "buzz"
-        }
         if (n % 3 == 0) && (n % 5 == 0) {
             r = "fizzbuzz"
+        } else if n % 3 == 0 {
+            r = "fizz"
+        } else if n % 5 == 0 {
+            r = "buzz"
+        } else {
+            r = "nope"
         }
 
         println!("{n}: {r}");
     }
 }
+
+fn using_match(user_input: u32) {
+    for x in 1..=user_input {
+        match (x % 3, x % 5) {
+            (0, 0) => println!("FizzBuzz"),
+            (0, _) => println!("Fizz"),
+            (_, 0) => println!("Buzz"),
+            _ => println!("{x}"),
+        }
+    }
+}
+// TODO: move user inputfunctiuonalityu into a separate function
---
+++ the_book/common_concepts/target/debug/deps/common_concepts-3dcd6171e79130ea.d
@@ -0,0 +1,5 @@
+/Users/antonshablyko/projects/rust/rustlang/the_book/common_concepts/target/debug/deps/common_concepts-3dcd6171e79130ea.rmeta: src/main.rs
+
+/Users/antonshablyko/projects/rust/rustlang/the_book/common_concepts/target/debug/deps/common_concepts-3dcd6171e79130ea.d: src/main.rs
+
+src/main.rs:
